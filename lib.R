131e4a07615bddc248f17e82749f721268c5a022
Sjur Øfsteng <sjurj@hotmail.com>
2025-02-10 15:21:57 +0100

    done 7-8

---
+++ lib.R
@@ -0,0 +1,17 @@
+
+
+
+# title: library#
+#
+# purpose: install rethinking and rstan
+#
+# install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"))
+# devtools::install_github("rmcelreath/rethinking")
+#
+# install.packages("cmdstanr", repos = c('https://stan-dev.r-universe.dev', getOption("repos")))
+
+
+##########################
+
+
+
